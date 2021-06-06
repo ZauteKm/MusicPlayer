cf00bfb80df640049d9ae805a92c3f9abd9c7244
Mr. Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-06-06 16:49:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+"""
+VCMusicPlayer, Telegram Voice Chat Userbot
+Copyright (C) 2021  Zaute Km
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
 from pyrogram import Client, idle, filters
 import os
 from threading import Thread
@@ -9,7 +26,7 @@ import asyncio
 
 CHAT=Config.CHAT
 bot = Client(
-    "Musicplayer",
+    "RadioPlayer",
     Config.API_ID,
     Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
@@ -29,7 +46,7 @@ bot.run(main())
 bot.start()
 @bot.on_message(filters.command("restart") & filters.user(Config.ADMINS))
 def restart(client, message):
-    message.reply_text("Restarting...")
+    message.reply_text("**Restarting... Join @AsmSafone!**")
     Thread(
         target=stop_and_restart
         ).start()
