a51821b96e09784b85644cb5e9583a788f7649b9
Mr. Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-06-06 17:05:31 +0530

    Update radio.py

--- plugins/radio.py
+++ plugins/radio.py
@@ -1,3 +1,21 @@
+"""
+VC Music Player, Telegram Voice Chat Userbot
+Copyright (C) 2021  Zaute Km | TGVCSETS
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from utils.vc import mp, RADIO
@@ -8,15 +26,15 @@ ADMINS=Config.ADMINS
 @Client.on_message(filters.command("radio") & filters.user(ADMINS))
 async def radio(client, message: Message):
     if 1 in RADIO:
-        await message.reply_text("Kindly stop existing Radio Stream /stopradio")
+        await message.reply_text("**Please Stop Existing Radio Stream /stopradio!**")
         return
     await mp.start_radio()
-    await message.reply_text(f"Started Radio: <code>{STREAM_URL}</code>")
+    await message.reply_text(f"**Started Radio:** <code>{STREAM_URL}</code>")
 
-@Client.on_message(filters.command('stopradio') & filters.user(ADMINS))
+@Client.on_message(filters.command("stopradio") & filters.user(ADMINS))
 async def stop(_, message: Message):
     if 0 in RADIO:
-        await message.reply_text("Kindly start Radio First /radio")
+        await message.reply_text("**Please Start A Radio Stream First /radio!**")
         return
     await mp.stop_radio()
-    await message.reply_text("Radio stream ended.")
+    await message.reply_text("**Radio Stream Ended Successfully!**")
