f1946b32711991d8e8290e01fb442af7fd397196
Mr. Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-06-11 15:21:27 +0530

    Update main.py
    Release v2.2

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
-
 from pyrogram import Client, idle, filters
 import os
 from threading import Thread
