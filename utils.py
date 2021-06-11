630f0f092b8032ad1c6f85ca9609fc57d38291cf
Mr. Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-06-11 15:19:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,7 +12,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
-
 import os
 from config import Config
 import ffmpeg
