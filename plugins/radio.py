46407ea3ec1facd6feb246406fbece16fab9c016
Mr. Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-06-11 13:38:43 +0530

    Update radio.py
    Release v2.2

--- plugins/radio.py
+++ plugins/radio.py
@@ -18,23 +18,24 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from utils.vc import mp, RADIO
+from utils import mp, RADIO
 from config import Config
-STREAM_URL=Config.STREAM_URL
+from config import STREAM
+
 ADMINS=Config.ADMINS
 
 @Client.on_message(filters.command("radio") & filters.user(ADMINS))
 async def radio(client, message: Message):
     if 1 in RADIO:
-        await message.reply_text("**Please Stop Existing Radio Stream /stopradio!**")
+        await message.reply_text("Kindly stop existing Radio Stream /stopradio")
         return
     await mp.start_radio()
-    await message.reply_text(f"**Started Radio:** <code>{STREAM_URL}</code>")
+    await message.reply_text(f"Started Radio: <code>{STREAM}</code>")
 
-@Client.on_message(filters.command("stopradio") & filters.user(ADMINS))
+@Client.on_message(filters.command('stopradio') & filters.user(ADMINS))
 async def stop(_, message: Message):
     if 0 in RADIO:
-        await message.reply_text("**Please Start A Radio Stream First /radio!**")
+        await message.reply_text("Kindly start Radio First /radio")
         return
     await mp.stop_radio()
-    await message.reply_text("**Radio Stream Ended Successfully!**")
+    await message.reply_text("Radio stream ended.")
