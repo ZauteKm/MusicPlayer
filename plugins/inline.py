5170bc5e6ad397846ee0a3ebbce0bd48799dd106
Mr. Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-06-11 13:33:07 +0530

    Create inline.py
    Version 2.2

---
+++ plugins/inline.py
@@ -0,0 +1,74 @@
+"""
+VC Music Player, Telegram Voice Chat Userbot
+Copyright (C) 2021  Zaute Km | TGVCSETS
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+from pyrogram.handlers import InlineQueryHandler
+from youtubesearchpython import VideosSearch
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
+from pyrogram import Client, errors
+
+
+@Client.on_inline_query()
+async def search(client, query):
+    answers = []
+    string = query.query.lower().strip().rstrip()
+
+    if string == "":
+        await client.answer_inline_query(
+            query.id,
+            results=answers,
+            switch_pm_text=("Search a youtube video"),
+            switch_pm_parameter="help",
+            cache_time=0
+        )
+        return
+    else:
+        videosSearch = VideosSearch(string.lower(), limit=50)
+        for v in videosSearch.result()["result"]:
+            answers.append(
+                InlineQueryResultArticle(
+                    title=v["title"],
+                    description=("Duration: {} Views: {}").format(
+                        v["duration"],
+                        v["viewCount"]["short"]
+                    ),
+                    input_message_content=InputTextMessageContent(
+                        "/play https://www.youtube.com/watch?v={}".format(
+                            v["id"]
+                        )
+                    ),
+                    thumb_url=v["thumbnails"][0]["url"]
+                )
+            )
+        try:
+            await query.answer(
+                results=answers,
+                cache_time=0
+            )
+        except errors.QueryIdInvalid:
+            await query.answer(
+                results=answers,
+                cache_time=0,
+                switch_pm_text=("Nothing found"),
+                switch_pm_parameter="",
+            )
+
+
+__handlers__ = [
+    [
+        InlineQueryHandler(
+            search
+        )
+    ]
+]
+
