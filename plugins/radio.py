0a5d002da4e73136aa5eb37d75a29bdd0066dd86
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-06-24 18:09:09 +0530

    Update radio.py

--- plugins/radio.py
+++ plugins/radio.py
@@ -15,27 +15,28 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from utils import mp, RADIO
+from utils import mp, RADIO, USERNAME
 from config import Config
 from config import STREAM
 
 ADMINS=Config.ADMINS
 
-@Client.on_message(filters.command("radio") & filters.user(ADMINS))
+@Client.on_message(filters.command(["radio", f"radio@{USERNAME}"]) & filters.user(ADMINS))
 async def radio(client, message: Message):
     if 1 in RADIO:
         await message.reply_text("Kindly stop existing Radio Stream /stopradio")
         return
     await mp.start_radio()
     await message.reply_text(f"Started Radio: <code>{STREAM}</code>")
+    await message.delete()
 
-@Client.on_message(filters.command('stopradio') & filters.user(ADMINS))
+@Client.on_message(filters.command(['stopradio', f"stopradio@{USERNAME}"]) & filters.user(ADMINS))
 async def stop(_, message: Message):
     if 0 in RADIO:
         await message.reply_text("Kindly start Radio First /radio")
         return
     await mp.stop_radio()
     await message.reply_text("Radio stream ended.")
+    await message.delete()
