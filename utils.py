74e1f21c847b6a32eecc0925446f6c1aef8b964d
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-06-24 18:15:02 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -41,9 +41,9 @@ bot = Client(
     bot_token=Config.BOT_TOKEN
 )
 bot.start()
+e=bot.get_me()
+USERNAME=e.username
 
-class DurationLimitError(Exception):
-    pass
 
 ydl_opts = {
     "format": "bestaudio[ext=m4a]",
@@ -55,23 +55,18 @@ ydl = YoutubeDL(ydl_opts)
 def youtube(url: str) -> str:
     info = ydl.extract_info(url, False)
     duration = round(info["duration"] / 60)
-
-    if duration > DURATION_LIMIT:
-        raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
-        )
     try:
         ydl.download([url])
-    except:
-        raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
-        )
+    except Exception as e:
+        print(e)
+        pass
     return path.join("downloads", f"{info['id']}.{info['ext']}")
 
 class MusicPlayer(object):
     def __init__(self):
         self.group_call = GroupCall(USER, path_to_log_file='')
         self.chat_id = None
+
 
     async def send_playlist(self):
         if not playlist:
@@ -128,44 +123,26 @@ class MusicPlayer(object):
         client = group_call.client
         raw_file = os.path.join(client.workdir, DEFAULT_DOWNLOAD_DIR,
                                 f"{song[1]}.raw")
-        if os.path.exists(raw_file):
-            os.remove(raw_file)
+        #if os.path.exists(raw_file):
+            #os.remove(raw_file)
         if not os.path.isfile(raw_file):
             # credits: https://t.me/c/1480232458/6825
-            os.mkfifo(raw_file)
+            #os.mkfifo(raw_file)
             if song[3] == "telegram":
                 original_file = await bot.download_media(f"{song[2]}")
-                ffmpeg.input(original_file).output(
-                    raw_file,
-                    format='s16le',
-                    acodec='pcm_s16le',
-                    ac=2,
-                    ar='48k',
-                    loglevel='error'
-                ).overwrite_output().run()
-                os.remove(original_file)
             elif song[3] == "youtube":
                 original_file = youtube(song[2])
-                ffmpeg.input(original_file).output(
-                    raw_file,
-                    format='s16le',
-                    acodec='pcm_s16le',
-                    ac=2,
-                    ar='48k',
-                    loglevel='error'
-                ).overwrite_output().run()
-                os.remove(original_file)
             else:
                 original_file=wget.download(song[2])
-                ffmpeg.input(original_file).output(
-                    raw_file,
-                    format='s16le',
-                    acodec='pcm_s16le',
-                    ac=2,
-                    ar='48k',
-                    loglevel='error'
-                ).overwrite_output().run()
-                os.remove(original_file)
+            ffmpeg.input(original_file).output(
+                raw_file,
+                format='s16le',
+                acodec='pcm_s16le',
+                ac=2,
+                ar='48k',
+                loglevel='error'
+            ).overwrite_output().run()
+            os.remove(original_file)
 
 
     async def start_radio(self):
@@ -173,13 +150,12 @@ class MusicPlayer(object):
         if group_call.is_connected:
             playlist.clear()   
             group_call.input_filename = ''
+            await group_call.stop()
         process = FFMPEG_PROCESSES.get(CHAT)
         if process:
             process.send_signal(signal.SIGTERM)
         station_stream_url = STREAM_URL
         group_call.input_filename = f'radio-{CHAT}.raw'
-        if not group_call.is_connected:
-            await mp.start_call()
         try:
             RADIO.remove(0)
         except:
@@ -191,7 +167,7 @@ class MusicPlayer(object):
         if os.path.exists(group_call.input_filename):
             os.remove(group_call.input_filename)
         # credits: https://t.me/c/1480232458/6825
-        os.mkfifo(group_call.input_filename)
+        #os.mkfifo(group_call.input_filename)
         process = ffmpeg.input(station_stream_url).output(
             group_call.input_filename,
             format='s16le',
@@ -200,12 +176,14 @@ class MusicPlayer(object):
             ar='48k'
         ).overwrite_output().run_async()
         FFMPEG_PROCESSES[CHAT] = process
+        while True:
+            if os.path.isfile(group_call.input_filename):
+                await group_call.start(CHAT)
+                break
+            else:
+                continue
 
-
-    
     async def stop_radio(self):
-        if 0 in RADIO:
-            return
         group_call = mp.group_call
         if group_call:
             playlist.clear()   
@@ -226,29 +204,7 @@ class MusicPlayer(object):
         group_call = mp.group_call
         await group_call.start(CHAT)
         
-    async def startupradio(self):
-        group_call = mp.group_call
-        if group_call:
-            group_call.stop_playout()
-            playlist.clear()
-        group_call.input_filename = f'radio-{CHAT}.raw'
-        process = FFMPEG_PROCESSES.get(CHAT)
-        if process:
-            process.send_signal(signal.SIGTERM)
-        station_stream_url = STREAM_URL
-        await group_call.start(CHAT)
-        try:
-            RADIO.add(1)
-        except:
-            pass
-        process = ffmpeg.input(station_stream_url).output(
-            group_call.input_filename,
-            format='s16le',
-            acodec='pcm_s16le',
-            ac=2,
-            ar='48k'
-        ).overwrite_output().run_async()
-        FFMPEG_PROCESSES[CHAT] = process
+
 
 
 mp = MusicPlayer()
