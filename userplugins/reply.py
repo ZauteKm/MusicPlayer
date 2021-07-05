f3958c75d52e0d49a94207360745dcd5b750575a
Zaute <77600757+Zaute-Km@users.noreply.github.com>
2021-07-05 17:47:36 +0530

    Update reply.py

--- userplugins/reply.py
+++ userplugins/reply.py
@@ -17,7 +17,7 @@ from utils import USERNAME
 from config import Config
 ADMINS=Config.ADMINS
 from pyrogram.errors import BotInlineDisabled
-@Client.on_message(filters.private & ~filters.bot & filters.incoming & ~filters.service)
+@Client.on_message(filters.private & ~filters.bot & filters.incoming & ~filters.service & ~filters.me)
 async def reply(client, message): 
     try:
         inline = await client.get_inline_bot_results(USERNAME, "ORU_MANDAN_PM_VANNU")
