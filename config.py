80b1a5edd76bbb9ae943bf4d22c16e94b2cf538e
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-03 07:17:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,16 +25,32 @@ ydl_opts = {
 ydl = YoutubeDL(ydl_opts)
 links=[]
 finalurl=""
-STREAM=os.environ.get("STREAM_URL", "https://eu10.fastcast4u.com/clubfmuae")
+C_PLAY=False
+Y_PLAY=False
+STREAM=os.environ.get("STREAM_URL", "https://t.me/tgbotsproject")
 regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+"
 match = re.match(regex,STREAM)
+regex_ = r"http.*"
+match_ = re.match(regex_,STREAM)
 if match:
     meta = ydl.extract_info(STREAM, download=False)
     formats = meta.get('formats', [meta])
     for f in formats:
         links.append(f['url'])
-    finalurl=links[0]
+    finalurl=links[-1]
+elif STREAM.startswith("https://t.me/tgbotsproject"):
+    try:
+        msg_id=STREAM.split("/", 4)[4]
+        finalurl=int(msg_id)
+        Y_PLAY=True
+    except:
+        finalurl="https://eu10.fastcast4u.com/clubfmuae"
+        print("Unable to fetch youtube playlist, starting CLUB FM")
+        pass
+elif match_:
+    finalurl=STREAM 
 else:
+    C_PLAY=True
     finalurl=STREAM
 
 class Config:
@@ -48,8 +64,12 @@ class Config:
     else:
         LOG_GROUP=None
     STREAM_URL=finalurl
+    CPLAY=C_PLAY
+    YPLAY=Y_PLAY
+    SHUFFLE=bool(os.environ.get("SHUFFLE", True))
+    DELETE_HISTORY=bool(os.environ.get("DELETE_HISTORY", True))
+    LIMIT=int(os.environ.get("LIMIT", 1500))
     ADMIN_ONLY=os.environ.get("ADMIN_ONLY", "N")
-    ARQ_API=os.environ.get("ARQ_API", "")
     REPLY_MESSAGE=os.environ.get("REPLY_MESSAGE", None)
     if REPLY_MESSAGE:
         REPLY_MESSAGE=REPLY_MESSAGE
@@ -61,7 +81,8 @@ class Config:
     DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 15))
     DELAY = int(os.environ.get("DELAY", 10))
     API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")     
     SESSION = os.environ.get("SESSION_STRING", "")
     playlist=[]
     msg = {}
+    CONV = {}
