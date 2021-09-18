b8e4d3699600230163f2b21943d3b51d6f1e4de1
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-18 19:43:16 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,25 +1,33 @@
-"""
-VC Music Player, Telegram Voice Chat Userbot
-Copyright (C) 2021  Zaute Km
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
+#!/usr/bin/env python3
+# Copyright (C) @ZauteKm
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from config import Config
 from pyrogram import Client
 from config import Config
-
-USER = Client(
-    Config.SESSION,
-    Config.API_ID,
-    Config.API_HASH,
-    plugins=dict(root="userplugins")
-    )
-
+REPLY_MESSAGE=Config.REPLY_MESSAGE
+if REPLY_MESSAGE is not None:
+    USER = Client(
+        Config.SESSION,
+        Config.API_ID,
+        Config.API_HASH,
+        plugins=dict(root="userplugins")
+        )
+else:
+    USER = Client(
+        Config.SESSION,
+        Config.API_ID,
+        Config.API_HASH
+        )
 USER.start()
+
