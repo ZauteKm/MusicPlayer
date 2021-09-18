68e0bea17f1786833a9a738955823f8a051c8872
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-18 19:44:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LushaiMusic/VCMusicPlayer.git /VCMusicPlayer
-cd /VCMusicPlayer
+git clone https://github.com/ZauteKm/Radio-Music-Bot.git /Radio-Music-Bot
+cd /Radio-Music-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
