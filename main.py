fdb5fa0d3359ad9f8977ee32588cfa4f34d70cde
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-18 20:00:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-"""
-VC Music Player, Telegram Voice Chat Userbot
-Copyright (C) 2021  Zaute Km
+#!/usr/bin/env python3
+# Copyright (C) @ZauteKm
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 try:
     import asyncio
     from pyrogram import Client, idle, filters
@@ -39,7 +37,7 @@ except ModuleNotFoundError:
 
 CHAT=Config.CHAT
 bot = Client(
-    "Musicplayer",
+    "Radio-Music-Bot",
     Config.API_ID,
     Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
