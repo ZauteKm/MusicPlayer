4a0fe88b1da3174b54b75b867a43a6ad638a7a77
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-19 07:03:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ except ModuleNotFoundError:
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 bot = Client(
-    "Radio-Music-Bot",
+    "RadioMusicBot",
     Config.API_ID,
     Config.API_HASH,
     bot_token=Config.BOT_TOKEN
@@ -89,7 +89,7 @@ if RADIO_TITLE=="NO":
 
 
 
-class Radio-Music-Bot(object):
+class RadioMusicBot(object):
     def __init__(self):
         self.group_call = GroupCallFactory(USER, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM).get_file_group_call()
 
@@ -512,7 +512,7 @@ class Radio-Music-Bot(object):
         return PROGRESS.get(int(user))
                 
 
-mp = Radio-Music-Bot()
+mp = RadioMusicBot()
 
 # pytgcalls handlers
 @mp.group_call.on_network_status_changed
