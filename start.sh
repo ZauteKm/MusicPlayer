be755849ea235daff3c6359b3e91ad78976d8bd6
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-11-08 22:31:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/Radio-Music-Bot.git /Radio-Music-Bot
-cd /Radio-Music-Bot
+git clone https://github.com/ZauteKm/MusicPlayer.git /MusicPlayer
+cd /MusicPlayer
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
